1697b2ee753034812981fcae8d3020c9e7e423c2
mines416 <tapiapaines@gmail.com>
2024-05-27 20:22:42 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,6 +4,10 @@ var i;
 
 function preload(){
   //loadImage de path (camino)
+  pathImg=loadImage("path.png");
+  boyImg=loadAnimation("Jake1.png", "Jake2.png");
+
+
   //loadAnimation de boy (niño)
  
 }
@@ -12,20 +16,31 @@ function setup(){
   
   createCanvas(400,400);
  //crear sprite de path (camino) 
+ path=createSprite(200,100);
+ path.addImage(pathImg);
+ path.scale=1.2;
+ 
+
+
 //agregar imagen de path
 //Hacer que la pista sea un fondo en movimiento al darle velocidad Y.
-path.scale=1.2;
+
 
 //crear sprite de boy (niño)
+boy=createSprite(200,350);
+boy.addAnimation("correr", boyImg);
+
 //agregar animación para boy
-boy.scale=0.08;
+boy.scale=0.5;
   
 // crear  left Boundary (límite izquierdo)
 leftBoundary=createSprite(0,0,100,800);
+leftBoundary.visible=false;
 ////establecer visibilidad como false (falso) para límite izquierdo
 
 //crear right Boundary (límite derecho)
 rightBoundary=createSprite(410,0,100,800);
+rightBoundary.visible=false;
 //establecer visibilidad como false (falso) para límite izquierdo
 }
 
@@ -34,15 +49,22 @@ function draw() {
   path.velocityY = 4;
   
   // boy moviéndose en el eje X con el mouse
+
+  boy.x=mouseX;
   
   edges= createEdgeSprites();
   boy.collide(edges[3]);
   // colisión de boy con los límites derecho e izquierdo invisibles 
+
+  boy.collide(leftBoundary);
+  boy.collide(rightBoundary);
   
   //código para reiniciar el fondo
   if(path.y > 400 ){
     path.y = height/2;
   }
+
+  
   
   drawSprites();
 }
